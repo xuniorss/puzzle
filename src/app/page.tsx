23b3f87ef880b61af6617d83fa3464311fa03b59
xuniorss <fortunatojunior2020@gmail.com>
2023-12-23 16:31:33 -0300

    fix: melhorando performance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import { cn } from '@/lib/utils'
 import { decrypt, encrypt } from '@/utils/crypto2'
 import { useCallback, useEffect, useState } from 'react'
 
@@ -9,11 +10,12 @@ export default function Home() {
 	const [inputText, setInputText] = useState('')
 	const [decryptedText, setDecryptedText] = useState('')
 	const [mounted, setMounted] = useState(false)
+	const [change, setChange] = useState({ number: 0, char: 0 })
 
 	const handleDecrypt = useCallback(() => {
-		const decrypted = decrypt(inputText, 3, 5)
+		const decrypted = decrypt(inputText, change.number, change.char)
 		setDecryptedText(decrypted)
-	}, [inputText])
+	}, [change.char, change.number, inputText])
 
 	useEffect(() => {
 		if (inputText === '' || inputText.length <= 0) setDecryptedText('')
@@ -30,11 +32,42 @@ export default function Home() {
 	return (
 		<section className="flex h-full w-full flex-col items-center justify-center">
 			<div className="w-full max-w-screen-sm space-y-4">
-				{numbers.map((n) => (
-					<div key={n}>
-						<span>{n}</span>
+				<div className="grid grid-cols-2 gap-x-4">
+					<h2 className="font-semibold text-white">Mudar letra a cada</h2>
+					<h2 className="font-semibold text-white">Mudar número a cada</h2>
+				</div>
+				<div className="grid grid-cols-2 gap-x-4">
+					<div className="grid grid-cols-5 gap-2 text-center">
+						{numbers.map((number) => (
+							<button
+								key={number}
+								className={cn(
+									'flex h-9 w-9 items-center justify-center rounded border-2 border-zinc-500 transition-all hover:bg-zinc-600',
+									change.char === number && 'bg-zinc-600',
+								)}
+								onClick={() =>
+									setChange((ch) => ({ ...ch, char: number }))
+								}
+							>
+								<span className="text-white">{number}</span>
+							</button>
+						))}
 					</div>
-				))}
+					<div className="grid grid-cols-5 gap-2 text-center">
+						{numbers.map((number) => (
+							<button
+								key={number}
+								className={cn(
+									'flex h-9 w-9 items-center justify-center rounded border-2 border-zinc-500 transition-all hover:bg-zinc-600',
+									change.number === number && 'bg-zinc-600',
+								)}
+								onClick={() => setChange((ch) => ({ ...ch, number }))}
+							>
+								<span className="text-white">{number}</span>
+							</button>
+						))}
+					</div>
+				</div>
 				<div className="flex gap-x-3">
 					<Input
 						type="text"
@@ -51,7 +84,7 @@ export default function Home() {
 							<h2 className="font-bold text-blue-400">Encrypted</h2>
 
 							<p className="break-words text-white">
-								{encrypt(inputText, 3, 5)}
+								{encrypt(inputText, change.number, change.char)}
 							</p>
 						</div>
 					</div>
--- src/utils/crypto2.ts
+++ src/utils/crypto2.ts
@@ -1,47 +1,53 @@
+const LOWERCASE_A_CODE = 'a'.charCodeAt(0)
+const UPPERCASE_A_CODE = 'A'.charCodeAt(0)
+const ZERO_CODE = '0'.charCodeAt(0)
+const ALPHABET_LENGTH = 26
+const NUMBERS_LENGTH = 10
+
+const shiftChar = (
+	char: string,
+	change: number,
+	baseCode: number,
+	length: number,
+): string =>
+	String.fromCharCode(
+		((char.charCodeAt(0) - baseCode + change + length) % length) + baseCode,
+	)
+
 const encryptChar = (
 	char: string,
-	changeNumber: number,
-	changeChar: number,
+	changeNumber: number = 0,
+	changeChar: number = 0,
 ): string => {
-	if (char >= 'a' && char <= 'z')
-		return String.fromCharCode(
-			((char.charCodeAt(0) - 97 + changeChar) % 26) + 97,
-		)
-	else if (char >= 'A' && char <= 'Z')
-		return String.fromCharCode(
-			((char.charCodeAt(0) - 65 + changeChar) % 26) + 65,
-		)
-	else if (char >= '0' && char <= '9')
-		return String.fromCharCode(
-			((char.charCodeAt(0) - 48 + changeNumber) % 10) + 48,
-		)
+	if (char >= 'a' && char <= 'z') {
+		return shiftChar(char, changeChar, LOWERCASE_A_CODE, ALPHABET_LENGTH)
+	} else if (char >= 'A' && char <= 'Z') {
+		return shiftChar(char, changeChar, UPPERCASE_A_CODE, ALPHABET_LENGTH)
+	} else if (char >= '0' && char <= '9') {
+		return shiftChar(char, changeNumber, ZERO_CODE, NUMBERS_LENGTH)
+	}
 	return char
 }
 
 const decryptChar = (
 	char: string,
-	changeNumber: number,
-	changeChar: number,
+	changeNumber: number = 0,
+	changeChar: number = 0,
 ): string => {
-	if (char >= 'a' && char <= 'z')
-		return String.fromCharCode(
-			((char.charCodeAt(0) - 97 - changeChar + 26) % 26) + 97,
-		)
-	else if (char >= 'A' && char <= 'Z')
-		return String.fromCharCode(
-			((char.charCodeAt(0) - 65 - changeChar + 26) % 26) + 65,
-		)
-	else if (char >= '0' && char <= '9')
-		return String.fromCharCode(
-			((char.charCodeAt(0) - 48 - changeNumber + 10) % 10) + 48,
-		)
+	if (char >= 'a' && char <= 'z') {
+		return shiftChar(char, -changeChar, LOWERCASE_A_CODE, ALPHABET_LENGTH)
+	} else if (char >= 'A' && char <= 'Z') {
+		return shiftChar(char, -changeChar, UPPERCASE_A_CODE, ALPHABET_LENGTH)
+	} else if (char >= '0' && char <= '9') {
+		return shiftChar(char, -changeNumber, ZERO_CODE, NUMBERS_LENGTH)
+	}
 	return char
 }
 
 export const encrypt = (
 	text: string,
-	changeNumber: number,
-	changeChar: number,
+	changeNumber: number = 0,
+	changeChar: number = 0,
 ): string =>
 	text
 		.split('')
@@ -50,8 +56,8 @@ export const encrypt = (
 
 export const decrypt = (
 	text: string,
-	changeNumber: number,
-	changeChar: number,
+	changeNumber: number = 0,
+	changeChar: number = 0,
 ): string =>
 	text
 		.split('')
